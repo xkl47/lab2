e20cb396e8581a7c2f55c7dee3a47c6bdc7be477
xkl47 <19358592766@163.com>
2024-10-30 19:29:53 +0800

    Add files via upload

---
+++ tb_avoid_latch.v
@@ -0,0 +1,24 @@
+`timescale 1ns/1ns
+
+module tb_avoid_latch()
+{
+    reg in1,in2,in3;
+    wire [7:0]out
+};
+
+initial  begin
+       
+
+end
+    
+
+    avoid_latch avoid_latch_inst
+    (
+        .in1(in1),
+        .in2(in2),
+        .in3(in3),
+        .out(out)
+
+    );
+
+end
